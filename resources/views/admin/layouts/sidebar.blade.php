fe829ce3cd9bbab85543a0f1f501af6dfbccabd9
bayu3prayitno <treeprayitno12@gmail.com>
2025-10-27 13:44:35 +0700

    fix bg sidebar admin

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -2,7 +2,7 @@
 <div x-show="sidebarOpen" @click="sidebarOpen = false" x-transition:enter="transition-opacity ease-linear duration-300"
     x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
     x-transition:leave="transition-opacity ease-linear duration-300" x-transition:leave-start="opacity-100"
-    x-transition:leave-end="opacity-0" class="fixed inset-0 bg-gray-900 bg-opacity-50 z-40 lg:hidden"
+    x-transition:leave-end="opacity-0" class="fixed inset-0 bg-black/50 backdrop-blur-sm transition-opacity z-40 lg:hidden"
     style="display: none;">
 </div>
 
